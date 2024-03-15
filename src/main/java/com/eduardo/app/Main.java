ce494af4821c992ded546af759f77b76f3859bd5
eduardo <eduardo@debian.domain>
2024-03-15 12:37:40 -0400

    Init Main Scene Design Basic

---
+++ src/main/java/com/eduardo/app/Main.java
@@ -0,0 +1,23 @@
+package com.eduardo.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/scene/main/MainScene.fxml"));
+        Parent root = loader.load();
+        root.getStylesheets().add("/css/style.css");
+        stage.setScene(new Scene(root, 600, 400));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/eduardo/app/controller/MenuComponentController.java
@@ -0,0 +1,15 @@
+package com.eduardo.app.controller;
+
+import javafx.event.ActionEvent;
+
+public class MenuComponentController {
+
+    public void onBtnMin(ActionEvent actionEvent) {
+        System.out.println("OnBtnMin");
+    }
+
+    public void onBtnClose(ActionEvent actionEvent) {
+        System.out.println("OnBtnClose");
+    }
+
+}
